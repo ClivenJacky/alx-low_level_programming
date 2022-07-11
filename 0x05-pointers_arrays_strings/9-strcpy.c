5823280b4510f231c319ce288701d083240fd06d
ClivenJacky <Jackymashimbhy@gmail.com>
2022-07-11 10:11:30 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,13 +6,15 @@
  * @src: copy from
  * Return: string
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src, size_t n)
 {
-	int i = 0;
+	size_t i;
 
-	for (; src[i] != '\0'; i++)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	
 		dest[i] = src[i];
-	}
+	for (; i < n; i++)
+		dest[i] = '\0';
+	
 	return (dest);
 }
